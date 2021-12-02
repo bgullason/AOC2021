4ecc089198c67c0e76d92ebaa0a2fbb065a8be92
Ben Gullason <bendgullason@gmail.com>
2021-12-02 11:59:38 -0700

    Day2

---
+++ Day2/Day2P1.py
@@ -0,0 +1,22 @@
+from csv import reader
+import collections
+
+data = []
+# open file in read mode
+with open('Day2\Input.csv', 'r', encoding='utf-8-sig') as csv_data:
+    csv_reader = reader(csv_data)
+    for row in csv_reader:
+        data.append(row[0])
+
+#      X  Y
+pos = [0, 0]
+
+for instruction in data:
+    if 'forward' in instruction:
+        pos[0] = pos[0] + int(''.join(i for i in instruction if i.isdigit()))
+    elif 'down' in instruction:
+        pos[1] = pos[1] + int(''.join(i for i in instruction if i.isdigit()))
+    elif 'up' in instruction:
+        pos[1] = pos[1] - int(''.join(i for i in instruction if i.isdigit()))
+
+print(f'\nHorizontal Pos: {pos[0]}\nDepth: {pos[1]}\nProduct: {pos[0]*pos[1]}\n')
